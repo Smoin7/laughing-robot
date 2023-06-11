4e04323757f77fa590d55268c8bc400b21d46160
Smoin7 <136230967+Smoin7@users.noreply.github.com>
2023-06-11 09:13:07 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+      <header className="App-logo">
+      <nav><a href="https://chat.openai.com/">Try ChatGPT</a>
+        </nav>
+        <img src="Octocat.png" className="App-logo" alt="logo" />         
+        
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
